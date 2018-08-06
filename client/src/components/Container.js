e9ae6acd52c3985538fae0a61241766cfbab733a
BlaiseAK <blaise.a.kress@gmail.com>
2018-08-06 14:37:14 -0400

    got semantic up and running for grid usage need to work on styling

---
+++ client/src/components/Container.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Container, Image, Grid, Segment} from 'semantic-ui-react';
+
+const ContainerFluid = props => (
+
+    <Grid>
+    <Grid.Row>
+      <Grid.Column width={}>
+      <h2>Hello Eric</h2>
+      </Grid.Column>
+      <Grid.Column>
+<h2>Hello Eric</h2>
+      </Grid.Column>
+
+      <Grid.Column>
+      <h2>Hello Eric</h2>
+      </Grid.Column>
+      <Grid.Column>
+      <h2>Hello Eric</h2>
+      </Grid.Column>
+      <Grid.Column>
+      <h2>Hello Eric</h2>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+
+);
+
+export default ContainerFluid;
