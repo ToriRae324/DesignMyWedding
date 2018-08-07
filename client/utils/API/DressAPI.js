59da7a9e93e681be2f77b28deea7b04e2c8b7f51
Eric Johnson <ejohn156@uncc.edu>
2018-08-07 09:44:44 -0400

    modified front end api

--- client/utils/API/DressAPI.js
+++ client/utils/API/DressAPI.js
@@ -1,1 +1,9 @@
 //etsy
+import axios from "axios"
+
+export default {
+    getDresses: function(searchObj) {
+    const queryURL = "https://api.nytimes.com/svc/search/v2/Dressesearch.json?api-key=304fa3fe81e44dc5a4950d7dddf66075&q=" + topic + "&begin_date=" + startYear + "0101&end_date=" + endYear + "0101";
+    return axios.get(queryURL)
+    }
+  };
