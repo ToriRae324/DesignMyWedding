59e060ca22fadafb8aba45818ec31a0de721df39
Victoria Paul <victoriapaul@CodeBook-Pro.local>
2018-08-08 10:55:34 -0400

    dresses with style

--- client/src/components/Dress/DressDetail.js
+++ client/src/components/Dress/DressDetail.js
@@ -1,16 +1,27 @@
 import React from "react";
 
 const DressDetail = props => (
-  <div>
-    <ul>
-    <li>title: {props.title}</li>
-    <li>url: {props.url}</li>
-    <li>photo: {props.photo}</li>
-    <li>price: {props.price} {props.currency}</li>
-    <li>currency_code: {props.currency}</li>
-    <li>shop: {props.shop}</li>
-    </ul>
+  <div className="ui raised link card"> 
+  <div className="content">
+    <i className="right floated like icon"></i>
+    <div className="header"><a href={props.url}>{props.title}</a></div>
+        <a class="image" alt="dress" href={props.url}>
+        <img src={props.photo} />
+    </a>
+    <div className="description">
+      <p></p>
+    </div>
   </div>
+  <div className="extra content">
+    <span className="left floated">
+        Seller: 
+      <a href={props.shopSite}> {props.shop}</a>
+    </span>
+    <span className="right floated">
+            Price: {props.price} {props.currency}
+        </span>
+  </div>
+</div>
 );
 
 export default DressDetail;
