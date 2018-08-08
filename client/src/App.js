d83e6e0b24fea63a3bdb55f3fee4f41373abb4bf
Eric Johnson <ejohn156@uncc.edu>
2018-08-08 11:04:38 -0400

    working with venue api pull and saving to db

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ContainerFluid from './components/Container';
+import ContainerFluid from './components/Landing';
+import Venue from "./components/Venue"
 
 class App extends Component {
   render() {
     return (
+      <div>
       <ContainerFluid>
       </ContainerFluid>
+      <Venue/>
+      </div>
     );
   }
 }
--- client/src/components/Venue/Venue.js
+++ client/src/components/Venue/Venue.js
@@ -3,6 +3,8 @@ import "./Venue.css";
 import API from "../../utils/API/VenueAPI"
 import DB from "../../utils/DB/VenueDB"
 import VenueResults from  "./VenueResults"
+import VenueItems from "./VenueItems"
+import { Card } from "semantic-ui-react"
 
 class Venue extends Component {
     state = {
@@ -15,17 +17,27 @@ class Venue extends Component {
                     venues: res.data.response.groups[0].items
                 }))
     }
+    saveVenue = (event) => {
+        const {name} = event.target
+        alert(name + " was saved")
+    }
 
     render(){
         return(
-            
                 <VenueResults>
                     {
                 this.state.venues.map(venue => {return(
-                <li>{venue.venue.name}</li>)})}
+                <VenueItems
+                    name={venue.venue.name}
+                    phone={venue.venue.contact.formattedPhone}
+                    address={venue.venue.location.address}
+                    rating={venue.venue.rating}
+                    photo={venue.venue.photos.groups[0]}
+                    onClick={this.saveVenue}
+                />)})}
                 </VenueResults>
-            
         )
     }
 }
 export default Venue;
+
