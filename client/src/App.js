73c46b8f3b2bb64c9a7432a42777e4f69015695d
Eric Johnson <ejohn156@uncc.edu>
2018-08-08 13:06:13 -0400

    save function working for venues

--- client/src/App.js
+++ client/src/App.js
@@ -10,17 +10,11 @@ class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-      <ContainerFluid>
-      </ContainerFluid>
-      <Venue/>
-=======
         {/* Header goes here */}
 
 
       
       <Browse />
->>>>>>> faaa5610b82ce3136d5a4179ec716052d9014027
       </div>
     );
   }
