87f0e5d12fc87c28eba0762298f5575628245634
Victoria Paul <victoriapaul@CodeBook-Pro.local>
2018-08-08 12:17:59 -0400

    sidebar and router added

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dress from "./components/Dress"
+import Browse from "./pages/Browse"
 
 class App extends Component {
   render() {
     return (
       <div>
+        {/* Header goes here */}
+
+
       
-      <Dress />
+      <Browse />
       </div>
     );
   }
