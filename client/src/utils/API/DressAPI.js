c44494f669ad703889ac4d104efed6991c7f2528
Eric Johnson <ejohn156@uncc.edu>
2018-08-12 18:43:10 -0400

    started working on calling apis in the backend

--- client/src/utils/API/DressAPI.js
+++ client/src/utils/API/DressAPI.js
@@ -1,6 +1,11 @@
 import axios from "axios"
 export default {
-  get: function(Query) {
-    return axios.get("/api/etsyDress");
+  get: function(query) {
+    const BASEURL = "https://openapi.etsy.com/v2/listings/active";
+        const API_KEY = "?api_key=bvnaauvi6o2lhj5i298bi71o";
+        const Category = "&category=weddings/clothing/";
+        const Params = "&limit=50&fields=title,price,currency_code,url&includes=Images(url_170x135),Shop";
+        return axios(BASEURL + API_KEY + Category + query + Params)
+        }
   }
-};
+
--- controllers/EtsyDressController.js
+++ controllers/EtsyDressController.js
@@ -1,15 +1,15 @@
 const request = require("request")
 module.exports = {
-    find: function (Query) {
+    find: function (query) {
         const BASEURL = "https://openapi.etsy.com/v2/listings/active";
         const API_KEY = "?api_key=bvnaauvi6o2lhj5i298bi71o";
         const Category = "&category=weddings/clothing/";
-        const Params = "&limit=50&fields=title,price,currency_code,url&includes=Images(url_170x135),Shop";
-
-        request(BASEURL + API_KEY + Category + Query + Params, function(err,res,body){
+        const Params = "&limit=2&fields=title,price,currency_code,url&includes=Images(url_170x135),Shop";
+        console.log(BASEURL + API_KEY + Category + "wedding_gown" + Params)
+        request(BASEURL + API_KEY + Category + "wedding_gown" + Params, function(err,res,body){
             if(err) 
             console.log(err)
-            console.log(res.json)
+            return(res.body)
         })
 
     }
