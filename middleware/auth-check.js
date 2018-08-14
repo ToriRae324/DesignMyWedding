0ceff61c11cdbf90b0c078578dd3569c142f6384
Eric Johnson <ejohn156@uncc.edu>
2018-08-14 13:27:09 -0400

    browse works with/without authentication

--- middleware/auth-check.js
+++ middleware/auth-check.js
@@ -3,19 +3,20 @@ const db = require('../models');
 const config = require('../config');
 
 module.exports = (req, res, next) => {
-    if(!req.headers.authorization) {
+    if(!req.body.token) {
         return res.status(401).end();
     }
 
     // get the last part from an authorization header string
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.body.token.split(' ')[1];
+    console.log(token + ":middleware")
 
     // decode the token using a scret key-phrase
     return jwt.verify(token, config.jwtSecret, (err, decoded) => {
         if (err) {return res.status(401).end();}
 
         const userId = decoded.sub;
-
+        console.log(userId + ":middleware")
         // check if the user exists by user id
         return db.User.findById(userId, (userErr, user) => {
             if (userErr || !user) {
